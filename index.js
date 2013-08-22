da651abfbb0663cff4ee483f5e7056d67a9d0006
Nathan Rajlich <nathan@tootallnate.net>
2013-08-22 14:08:30 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+/**
+ * Module dependencies.
+ */
+
+var global = require('global')();
+
+/**
+ * Module exports.
+ *
+ * Logic borrowed from Modernizr:
+ *
+ *   - https://github.com/Modernizr/Modernizr/blob/master/feature-detects/cors.js
+ */
+
+module.exports = 'XMLHttpRequest' in global && 'withCredentials' in new XMLHttpRequest();
